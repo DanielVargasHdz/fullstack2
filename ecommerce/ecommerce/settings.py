95a13cebf6770617698627a5f743d737f277be71
DanielVargasHdz <161332191+DanielVargasHdz@users.noreply.github.com>
2024-06-12 00:09:36 -0600

    Social Login
    Hay que hardcodear usuarios

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -43,10 +43,45 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'django.contrib.sites',  # Necesario para django-allauth
 
+    # Aplicaciones de terceros
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+# Configuración de django-allauth
+ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+
+# Configuración de los proveedores sociales
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
+        'APP': {
+            'client_id': '416412137997-0ab96tt8gklqv81ge9hh863hrr7pv53u.apps.googleusercontent.com',
+            'secret': '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',
+            'key': ''
+        }
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,6 +91,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',  # Añadir esta línea
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+# ecommerce/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -29,7 +33,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
     
-    
+    path('social/', include('allauth.urls')), 
 
     
     
